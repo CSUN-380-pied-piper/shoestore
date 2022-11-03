3387448e931b4bdd41957a84cedba293bd082ed2
Megumi Ikeda <78073549+megumiikeda-1@users.noreply.github.com>
2022-11-02 17:07:44 -0700

    Add files via upload

---
+++ src/main/java/backend/SceneController.java
@@ -0,0 +1,31 @@
+package shoeStore;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneController {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToProductScene(ActionEvent event) throws IOException{
+		
+	}
+	
+	public void switchToShoppingCartScene(ActionEvent event) throws IOException{
+		
+	}
+	
+	public void switchToCheckOutScene(ActionEvent event) throws IOException{
+		
+	}
+	
+	public void switchToConfirmationScene(ActionEvent event) throws IOException{
+		
+	}
+	
+}
