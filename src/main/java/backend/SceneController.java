17f0c4c43f9d3f9b9347fb524bc799214500107f
Megumi Ikeda <78073549+megumiikeda-1@users.noreply.github.com>
2022-11-02 17:24:00 -0700

    Update SceneController.java

--- src/main/java/backend/SceneController.java
+++ src/main/java/backend/SceneController.java
@@ -14,7 +14,7 @@ public class SceneController {
 	
 	public void switchToProductScene(ActionEvent event) throws IOException{
 		/*
-		Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/shoestore.fxml"));
         	Scene scene = new Scene(root);
 		        stage.setScene(scene);
         	stage.setMinWidth(350);
@@ -26,7 +26,16 @@ public class SceneController {
 	}
 	
 	public void switchToShoppingCartScene(ActionEvent event) throws IOException{
-		
+		/*
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/shoppingCart.fxml"));
+        	Scene scene = new Scene(root);
+		        stage.setScene(scene);
+        	stage.setMinWidth(350);
+        	stage.setMinHeight(510);
+        	stage.setResizable(true);
+        	stage.initStyle(StageStyle.UNIFIED);
+        	stage.setTitle("Product");
+		*/
 	}
 	
 	public void switchToCheckOutScene(ActionEvent event) throws IOException{
