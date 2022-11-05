0f3c10fd992961050e1aea8821263472192f3693
Jocelyn <jocelynmallon@users.noreply.github.com>
2022-11-04 17:08:34 -0700

    Cleanup package structure and class names

--- src/main/java/gui/ShoeStoreController.java
+++ src/main/java/gui/ShoeStoreController.java
@@ -1,4 +1,4 @@
-package sample;
+package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -6,12 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Controller {
+public class ShoeStoreController {
 
     
     private Stage stage;
@@ -21,29 +20,36 @@ public class Controller {
     @FXML void addToCart(ActionEvent event) {
 
     }
+    @FXML
     public void switchToCheckout(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("checkout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/checkout.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
     public void switchToShoppingCart(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("shoppingCart.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/shoppingCart.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
     public void switchToOrderPlaced(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("orderPlaced.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/orderPlaced.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
     public void switchToShoeStore(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("shoeStore.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/shoeStore.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
