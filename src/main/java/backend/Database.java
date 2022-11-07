a90306cdf7f0dc9795171052fbd16d3ed36f0bfb
Jocelyn <jocelynmallon@users.noreply.github.com>
2022-11-07 13:57:04 -0800

    Begin implementing db connector

--- src/main/java/backend/Database.java
+++ src/main/java/backend/Database.java
@@ -8,23 +8,29 @@ public class Database {
 
     private final String url = "jdbc:postgresql://bgapytnwjskfjif8rjg3-postgresql.services.clever-cloud.com:5432/bgapytnwjskfjif8rjg3";
     private final String user = "udaqis555zzuvohu7keb";
+    private final String password = "paste_password_here";
 
     private Connection conn;
 
     public Database() {
-        this.conn = connect();
     }
 
-
-    public Connection connect() {
+    private void openConnection() {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
+            this.conn = conn;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
-        return conn;
+    }
+
+    public Connection connect() {
+        if (this.conn == null) {
+            this.openConnection();
+        }
+        return this.conn;
     }
 }
