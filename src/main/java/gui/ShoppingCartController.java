7a48eb2c36a68a7f161c3101aa1e82337f9d4738
kathie-liu <116608741+kathie-liu@users.noreply.github.com>
2022-11-09 17:56:39 -0800

    update ShoppingCardController
    add checkout button scene

--- src/main/java/gui/ShoppingCartController.java
+++ src/main/java/gui/ShoppingCartController.java
@@ -5,15 +5,20 @@ import backend.ShoppingCart;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import state.AppState;
+
+import java.io.IOException;
 import java.util.Stack;
 
 public class ShoppingCartController {
 
     private AppState state;
     private Stage stage;
+
+    private Scene scene;
     private Database db;
     private SceneLoader loader;
     private Stack<Parent> viewStack;
@@ -33,6 +38,14 @@ public class ShoppingCartController {
     @FXML public void backToStore(ActionEvent event) {
         Parent prevScene = viewStack.pop();
         stage.getScene().setRoot(prevScene);
+    }
+
+    @FXML
+    public void checkout(ActionEvent event) throws IOException {
+        Parent root = loader.load(getClass().getResource("/checkout.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
