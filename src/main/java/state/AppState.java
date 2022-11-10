738919f699d4057a426d3a1303d7e9d405cad740
Jocelyn <jocelynmallon@users.noreply.github.com>
2022-11-09 17:36:23 -0800

    Add javadoc stubs for appstate
    Begin adding the javadoc stubs and documentation for our AppState class

--- src/main/java/state/AppState.java
+++ src/main/java/state/AppState.java
@@ -7,6 +7,9 @@ import javafx.stage.Stage;
 
 import java.util.Stack;
 
+/**
+ * The type App state.
+ */
 public class AppState {
 
     private Stage stage;
@@ -14,6 +17,14 @@ public class AppState {
     private SceneLoader loader;
     private Stack<Parent> viewStack;
 
+    /**
+     * Instantiates a new App state.
+     *
+     * @param stage     the main stage of our application
+     * @param db        the db object that connects to our postgres db
+     * @param viewStack the view stack that keeps track of scenes so
+     *                      that users can navigate back & forward
+     */
     public AppState(Stage stage,
                     Database db,
                     Stack<Parent> viewStack) {
@@ -22,24 +33,49 @@ public class AppState {
         this.viewStack = viewStack;
     }
 
+    /**
+     * Gets stage.
+     *
+     * @return the stage
+     */
     public Stage getStage() {
         return stage;
     }
 
+    /**
+     * Gets db.
+     *
+     * @return the db
+     */
     public Database getDb() {
         return db;
     }
 
+    /**
+     * Gets our custom fxml/scene loader object.
+     *
+     * @return the loader
+     */
     public SceneLoader getLoader() {
         return loader;
     }
 
+    /**
+     * Sets our custom fxml/scene loader object.
+     *
+     * @param loader the loader
+     */
     public void setLoader(SceneLoader loader) {
         if (this.loader == null) {
             this.loader = loader;
         }
     }
 
+    /**
+     * Gets view stack.
+     *
+     * @return the view stack
+     */
     public Stack<Parent> getViewStack() {
         return viewStack;
     }
