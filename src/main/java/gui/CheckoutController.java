f679bbad231bc3a90b99ac3db74abbca7413f9bc
kathie-liu <116608741+kathie-liu@users.noreply.github.com>
2022-11-09 17:54:53 -0800

    update CheckoutController
    add some skeleton code

--- src/main/java/gui/CheckoutController.java
+++ src/main/java/gui/CheckoutController.java
@@ -1,4 +1,51 @@
 package gui;
 
+import backend.Database;
+import backend.ShoppingCart;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import state.AppState;
+
+import java.io.IOException;
+import java.util.Stack;
+
 public class CheckoutController {
+    private AppState state;
+    private Stage stage;
+    private Scene scene;
+    private Database db;
+    private SceneLoader loader;
+    private Stack<Parent> viewStack;
+
+    @FXML
+    Button placeOrderButton, backToStore, backToShoppingCart;
+
+    public CheckoutController(AppState state) {
+        this.state = state;
+    }
+
+    @FXML
+    public void backToShoppingCart(ActionEvent event) {
+        Parent prevScene = viewStack.pop();
+        stage.getScene().setRoot(prevScene);
+    }
+
+    @FXML
+    public void backToStore(ActionEvent event) throws IOException {
+        Parent root = loader.load(getClass().getResource("/shoeStore.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void initialize() {
+        this.db = state.getDb();
+        this.loader = state.getLoader();
+        this.stage = state.getStage();
+    }
 }
