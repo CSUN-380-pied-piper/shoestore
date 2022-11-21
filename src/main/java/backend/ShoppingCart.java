9aeccb05ba9be78a8aab2f459f3ec6c40cfaa408
Megumi Ikeda <78073549+megumiikeda-1@users.noreply.github.com>
2022-11-21 00:13:47 -0800

    Update ShoppingCart.java

--- src/main/java/backend/ShoppingCart.java
+++ src/main/java/backend/ShoppingCart.java
@@ -28,4 +28,43 @@ public class ShoppingCart {
     public double getTotal() {
         return total;
     }
+    
+    public String ReceiptInTextArea() {
+    	int HeelNum = 0;
+    	int SneakerNum = 0;
+    	int SandalNum = 0;
+    	int BootNum = 0;
+    	
+    	String finalText = "";
+    	
+    	for (int i = 0; i < contents.size(); i++) {
+    		if (contents.get(i).getName().equals("Heels")) {
+    			HeelNum++;
+    		} else if (contents.get(i).getName().equals("Sneakers")) {
+    			SneakerNum++;
+    		} else if (contents.get(i).getName().equals("Sandals")) {
+    			SandalNum++;
+    		} else if (contents.get(i).getName().equals("Boots")) {
+    			BootNum++;
+    		}
+    		
+    		if (HeelNum > 0) {
+    			finalText = "Number of Heels:  "+ HeelNum;
+    		}
+    		if (SneakerNum > 0) {
+    			finalText = finalText + "\nNumber of Sneakers: " + SneakerNum;
+    		}
+    		if (SandalNum > 0) {
+    			finalText = finalText + "\nNumber of Sandals: " + SandalNum;
+    		}
+    		if (BootNum > 0) {
+    			finalText = finalText + "\nNumber of Sandals: " + BootNum;
+    		}
+    		
+    		finalText = finalText + "\n\nTotal: $" + total;
+    		
+    	}
+    	return finalText;
+    	
+    }
 }
