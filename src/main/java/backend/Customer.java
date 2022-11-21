d9f4ddcfa65175515d561d2940749ecdefda9dbc
kathie-liu <116608741+kathie-liu@users.noreply.github.com>
2022-11-21 01:48:21 -0800

    Update Customer.java

--- src/main/java/backend/Customer.java
+++ src/main/java/backend/Customer.java
@@ -1,4 +1,19 @@
 package backend;
 
 public class Customer {
+    private String lastName, firstName, email, street, unit, city, state, cardholder;
+    private int phoneNum, zip, creditCardNum, expDate, cvcCode;
+
+    public Customer(String fn, String ln, int pn, String e, String st, String u, String c, String s, int z) {
+        firstName = fn;
+        lastName = ln;
+        phoneNum = pn;
+        email = e;
+        street = st;
+        unit = u;
+        city = c;
+        state = s;
+        zip = z;
+    }
+
 }
