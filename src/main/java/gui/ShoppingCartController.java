5cdaf2d0f424de2dd32e5fa07e411039ce770c0a
kathie-liu <116608741+kathie-liu@users.noreply.github.com>
2022-11-21 02:14:53 -0800

    Update ShoppingCartController.java

--- src/main/java/gui/ShoppingCartController.java
+++ src/main/java/gui/ShoppingCartController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import state.AppState;
 
