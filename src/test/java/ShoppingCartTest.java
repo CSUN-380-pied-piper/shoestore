bd5ea62141208f50612ba3ee29a62b3d848e1cb7
Jocelyn <jocelynmallon@users.noreply.github.com>
2022-11-23 16:53:12 -0800

    Add shopping cart test class
    Add shopping cart test class, using Junit 5

---
+++ src/test/java/ShoppingCartTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @org.junit.jupiter.api.Test
+    void addItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSubTotal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTax() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFinalTotal() {
+    }
+}
