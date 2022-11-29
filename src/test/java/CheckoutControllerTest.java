ce45eda601991f755a9ecbafd0d82ed9890b9642
kathie-liu <116608741+kathie-liu@users.noreply.github.com>
2022-11-28 18:16:52 -0800

    update Test Case
    add code to all other tests

--- src/test/java/CheckoutControllerTest.java
+++ src/test/java/CheckoutControllerTest.java
@@ -1,3 +1,4 @@
+import backend.Customer;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,5 +7,25 @@ class CheckoutControllerTest {
 
     @Test
     void placeOrder() {
+        Customer c = new Customer("John", "Doe", "1234567789",  "abc@gmail.com",
+                "123 Main St", "Ste C", "Alhambra", "CA", 91755);
+        assertTrue(c.getFirstName().equals("John"));
+        assertFalse(c.getFirstName().equals("Joh"));
+        assertTrue(c.getLastName().equals("Doe"));
+        assertFalse(c.getLastName().equals("Do"));
+        assertTrue(c.getPhoneNum().equals("1234567789"));
+        assertFalse(c.getPhoneNum().equals("987654321"));
+        assertTrue(c.getEmail().equals("abc@gmail.com"));
+        assertFalse(c.getEmail().equals("efg@gmail.com"));
+        assertTrue(c.getStreet().equals("123 Main St"));
+        assertFalse(c.getStreet().equals("432 1th St"));
+        assertTrue(c.getUnit().equals("Ste C"));
+        assertFalse(c.getUnit().equals(""));
+        assertTrue(c.getCity().equals("Alhambra"));
+        assertFalse(c.getCity().equals("San Diego"));
+        assertTrue(c.getState().equals("CA"));
+        assertFalse(c.getState().equals("NY"));
+        assertTrue(c.getZip() == 91755);
+        assertFalse(c.getZip() == 92004);
     }
 }
--- src/test/java/ShoppingCartTest.java
+++ src/test/java/ShoppingCartTest.java
@@ -20,10 +20,27 @@ class ShoppingCartTest {
 
     @org.junit.jupiter.api.Test
     void removeItem() {
+        Product sneakers = new Product("Sneakers",120.0, true, 5, 13);
+        sneakers.lastSizeProp().set(6.5);
+        sneakers.lastQtyProp().set(1);
+        cart.addItem(sneakers);
+        cart.removeItem(sneakers);
+        assertFalse(cart.getContents().contains(sneakers));
+        assertTrue(cart.getTax() == 0.00);
+        assertEquals(0.00, cart.getFinalTotal());
     }
 
     @org.junit.jupiter.api.Test
     void getSubTotal() {
+        Product boots = new Product("Boots",135.0, true, 5, 13);
+        boots.lastSizeProp().set(7.0);
+        boots.lastQtyProp().set(1);
+        cart.addItem(boots);
+        assertEquals(135, cart.getSubTotal());
+
+        // not working if test by following, subtotal = 0
+//        cart.subTotalProperty().set(145.87);
+//        assertEquals(145.87, cart.getSubTotal());
     }
 
     @org.junit.jupiter.api.Test
@@ -34,5 +51,7 @@ class ShoppingCartTest {
 
     @org.junit.jupiter.api.Test
     void getFinalTotal() {
+        cart.subTotalProperty().set(125.0);
+        assertEquals(134.06, cart.getFinalTotal(), .01);
     }
 }
