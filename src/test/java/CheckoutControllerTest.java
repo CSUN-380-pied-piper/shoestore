90c8fcc347e4b53d086ceb8ce1d36141d6686a21
Jocelyn <jocelynmallon@users.noreply.github.com>
2022-11-30 15:46:21 -0800

    Add Database test class skeleton

--- src/test/java/CheckoutControllerTest.java
+++ src/test/java/CheckoutControllerTest.java
@@ -1,6 +1,5 @@
 import backend.Customer;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckoutControllerTest {
---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @Test
+    void getCustomer() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+}
